df64a686a9fc33e9fdbab23bd291953a86b91a64
yahayagodiwa <yahayagodiwa8@gmail.com>
2024-12-15 19:52:34 +0100

    hero button z index set to 0

--- src/components/Botton.jsx
+++ src/components/Botton.jsx
@@ -3,7 +3,7 @@
 const Button = () => {
   return (
     <div className="relative group">
-      <div className="relative w-64 h-14 opacity-90 overflow-hidden rounded-xl bg-[#ffffff] z-10">
+      <div className="relative w-64 h-14 opacity-90 overflow-hidden rounded-xl bg-[#ffffff] z-0">
         <div className="absolute z-10 -translate-x-44 group-hover:translate-x-[30rem] 
         ease-in transistion-all duration-700 h-full w-44 bg-gradient-to-r from-[#d3d0da] to-white/10 opacity-30 -skew-x-12" />
         <div className="absolute flex items-center justify-center text-white z-[1] opacity-90 rounded-2xl inset-0.5 bg-black">
