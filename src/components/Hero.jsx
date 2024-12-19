594b11d64ea524dcc239045c439fbe683bba582c
yahayagodiwa <yahayagodiwa8@gmail.com>
2024-12-19 13:52:15 +0100

    testing1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,8 @@ export const Hero = () => {
         <h1 className='text-[50px] md:text-[60px] font-bold leading-tight'>More than just shorter links</h1>
         <p className='text-xl w-full md:w-[40vw]  py-5'>Build your brand’s recognition and get detailed insights 
         on how your links are performing.</p>
-       <a href='#main' className='text-center inline-block'><Button /> </a> 
+       <a href='https://www.profitablecpmrate.com/m5fftcrwd?key=c6f66485ea0cb9f001c0416e234037b0' 
+       className='text-center inline-block'><Button /> </a> 
         </div>
         <div>
             <img src={illust} alt="working" className='h-[300px] md:h-[400px] w-[90vw] md:w-[50vw]'/>
